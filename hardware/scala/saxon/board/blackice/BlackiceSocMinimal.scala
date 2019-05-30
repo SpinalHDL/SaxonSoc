e01bc28a40de82ba8f3600a8e0087a27ee948405
lawrie <lawrie.griffiths@ntlworld.com>
2019-05-31 00:12:17 +0100

    Start of minimal system for Blackice II board

---
+++ hardware/scala/saxon/board/blackice/BlackiceSocMinimal.scala
@@ -0,0 +1,90 @@
+package saxon.board.blackice
+
+import saxon.ResetSourceKind.EXTERNAL
+import saxon._
+import spinal.core._
+import spinal.lib.com.uart.UartCtrlMemoryMappedConfig
+import spinal.lib.generator._
+import spinal.lib.io.{Gpio, InOutWrapper}
+
+case class BlackicePll() extends BlackBox{
+  setDefinitionName("blackice_pll")
+  val clock_in = in Bool()
+  val clock_out = out Bool()
+  val locked = out Bool()
+}
+
+class BlackiceSocMinimalSystem extends BmbApbVexRiscvGenerator{
+  //Add components
+  val ramA = BmbOnChipRamGenerator(0x80000000l)
+  val uartA = Apb3UartGenerator(0x10000)
+  val gpioA = Apb3GpioGenerator(0x00000)
+
+  ramA.dataWidth.load(32)
+
+  //Interconnect specification
+  interconnect.addConnection(
+    cpu.iBus -> List(ramA.bmb),
+    cpu.dBus -> List(ramA.bmb)
+  )
+}
+
+
+class BlackiceSocMinimal extends Generator{
+  val clockCtrl = ClockDomainGenerator()
+  clockCtrl.resetSourceKind.load(EXTERNAL)
+  clockCtrl.powerOnReset.load(true)
+  clockCtrl.clkFrequency.load(50 MHz)
+
+  val system = new BlackiceSocMinimalSystem
+  system.onClockDomain(clockCtrl.clockDomain)
+
+  val clocking = add task new Area{
+    val CLOCK_100 = in Bool()
+    val GRESET = in Bool()
+
+    val pll = BlackicePll()
+    pll.clock_in := CLOCK_100
+
+    clockCtrl.clock.load(pll.clock_out)
+    clockCtrl.reset.load(!GRESET)
+  }
+}
+
+object BlackiceSocMinimalSystem{
+  def default(g : BlackiceSocMinimalSystem, clockCtrl : ClockDomainGenerator) = g {
+    import g._
+
+    cpu.config.load(VexRiscvConfigs.minimal)
+    cpu.enableJtag(clockCtrl)
+
+    ramA.size.load(12 KiB)
+    ramA.hexInit.load(null)
+
+    uartA.parameter load UartCtrlMemoryMappedConfig(
+      baudrate = 115200,
+      txFifoDepth = 1,
+      rxFifoDepth = 1
+    )
+
+    gpioA.parameter load Gpio.Parameter(width = 8)
+
+    g
+  }
+}
+
+
+object BlackiceSocMinimal {
+  //Function used to configure the SoC
+  def default(g : BlackiceSocMinimal) = g{
+    import g._
+    BlackiceSocMinimalSystem.default(system, clockCtrl)
+    g
+  }
+
+  //Generate the SoC
+  def main(args: Array[String]): Unit = {
+    SpinalRtlConfig.generateVerilog(IceStormInOutWrapper(default(new BlackiceSocMinimal()).toComponent()))
+  }
+}
+
---
+++ hardware/scala/saxon/board/blackice/IceStormInOutWrapper.scala
@@ -0,0 +1,78 @@
+package saxon.board.blackice
+
+import spinal.core._
+import spinal.lib.master
+import spinal.lib.io._
+
+import scala.collection.mutable
+
+case class SB_IO(pinType : String) extends BlackBox{
+  addGeneric("PIN_TYPE", B(pinType))
+  val PACKAGE_PIN = inout(Analog(Bool))
+  val OUTPUT_ENABLE = in(Bool)
+  val D_OUT_0 = in(Bool)
+  val D_IN_0 = out(Bool)
+  setDefinitionName("SB_IO")
+}
+
+object IceStormInOutWrapper {
+  def apply[T <: Component](c : T) : T = {
+    val dataParents = mutable.LinkedHashMap[Data,Int]()
+    def add(that : Data): Unit ={
+      if(that.parent != null){
+        dataParents(that.parent) = dataParents.getOrElseUpdate(that.parent,0) + 1
+        add(that.parent)
+      }
+    }
+    for(io <- c.getAllIo){
+      add(io)
+    }
+
+    c.rework {
+      for ((dataParent, count) <- dataParents) {
+        dataParent match {
+          case bundle: TriState[_]  if bundle.writeEnable.isOutput  => {
+            val newIo = inout(Analog(bundle.dataType)).setWeakName(bundle.getName())
+            bundle.setAsDirectionLess.unsetName().allowDirectionLessIo
+            bundle.read.assignFrom(newIo)
+            when(bundle.writeEnable){
+              newIo := bundle.write
+            }
+          }
+          case bundle : TriStateOutput[_] if bundle.isOutput => {
+            val newIo = inout(Analog(bundle.dataType)).setWeakName(bundle.getName())
+            bundle.setAsDirectionLess.unsetName().allowDirectionLessIo
+            when(bundle.writeEnable){
+              newIo := bundle.write
+            }
+          }
+          case bundle: ReadableOpenDrain[_]  if bundle.isMasterInterface => {
+            val newIo = inout(Analog(bundle.dataType)).setWeakName(bundle.getName())
+            bundle.setAsDirectionLess.unsetName().allowDirectionLessIo
+            bundle.read.assignFrom(newIo)
+            for((value, id) <- bundle.write.asBits.asBools.zipWithIndex) {
+              when(!value){
+                newIo.assignFromBits("0", id, 1 bits)
+              }
+            }
+          }
+          case bundle: TriStateArray if bundle.writeEnable.isOutput => {
+            val newIo = inout(Analog(bundle.write)).setWeakName(bundle.getName())
+            bundle.setAsDirectionLess.unsetName().allowDirectionLessIo
+            for(i <- 0 until bundle.width) {
+              val sbio = SB_IO("101001")
+              sbio.PACKAGE_PIN := newIo(i)
+              sbio.OUTPUT_ENABLE := bundle.writeEnable(i)
+              sbio.D_OUT_0 := bundle.write(i)
+              bundle.read(i) := sbio.D_IN_0
+            }
+          }
+          case _ =>
+        }
+      }
+    }
+    c
+  }
+}
+
+
