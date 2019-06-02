9136e987bcc7a5ae44f80fdb60b19a3f9799165b
lawrie <lawrie.griffiths@ntlworld.com>
2019-06-02 16:08:02 +0100

    Start of minimal system for the TinyFPGA BX board

---
+++ hardware/scala/saxon/board/tinyfpgabx/TinyFpgaBxSocMinimal.scala
@@ -0,0 +1,92 @@
+package saxon.board.tinyfpgabx
+
+import saxon._
+import spinal.core._
+import spinal.lib.com.uart.UartCtrlMemoryMappedConfig
+import spinal.lib.generator._
+import spinal.lib.io.{Gpio, InOutWrapper}
+import saxon.board.blackice.IceStormInOutWrapper
+
+case class TinyFpgaBxPll() extends BlackBox{
+  setDefinitionName("tinyfpgabx_pll")
+  val clock_in = in Bool()
+  val clock_out = out Bool()
+  val locked = out Bool()
+}
+
+class TinyFpgaBxSocMinimalSystem extends BmbApbVexRiscvGenerator{
+  //Add components
+  val ramA = BmbOnChipRamGenerator(0x80000000l)
+  val uartA = Apb3UartGenerator(0x10000)
+  val gpioA = Apb3GpioGenerator(0x00000)
+
+  ramA.dataWidth.load(32)
+
+  //Interconnect specification
+  interconnect.addConnection(
+    cpu.iBus -> List(ramA.bmb),
+    cpu.dBus -> List(ramA.bmb)
+  )
+}
+
+
+class TinyFpgaBxSocMinimal extends Generator{
+  val clockCtrl = ClockDomainGenerator()
+  clockCtrl.resetHoldDuration.load(255)
+  clockCtrl.resetSynchronous.load(false)
+  clockCtrl.powerOnReset.load(true)
+  clockCtrl.clkFrequency.load(20 MHz)
+
+  val system = new TinyFpgaBxSocMinimalSystem
+  system.onClockDomain(clockCtrl.clockDomain)
+
+  val clocking = add task new Area{
+    val CLOCK_16 = in Bool()
+    val GRESET = in Bool()
+
+    val pll = TinyFpgaBxPll()
+    pll.clock_in := CLOCK_16
+
+    clockCtrl.clock.load(pll.clock_out)
+    clockCtrl.reset.load(GRESET)
+  }
+}
+
+object TinyFpgaBxSocMinimalSystem{
+  def default(g : TinyFpgaBxSocMinimalSystem, clockCtrl : ClockDomainGenerator) = g {
+    import g._
+
+    cpu.config.load(VexRiscvConfigs.minimal)
+    cpu.enableJtag(clockCtrl)
+
+    ramA.size.load(12 KiB)
+    ramA.hexInit.load("hardware/scala/saxon/board/tinyfpgabx/blinkAndEcho.hex")
+
+    uartA.parameter load UartCtrlMemoryMappedConfig(
+      baudrate = 115200,
+      txFifoDepth = 1,
+      rxFifoDepth = 1
+    )
+
+    gpioA.parameter load Gpio.Parameter(width = 8)
+
+    g
+  }
+}
+
+
+object TinyFpgaBxSocMinimal {
+  //Function used to configure the SoC
+  def default(g : TinyFpgaBxSocMinimal) = g{
+    import g._
+    TinyFpgaBxSocMinimalSystem.default(system, clockCtrl)
+    clockCtrl.resetSensitivity load(ResetSensitivity.FALL)
+    g
+  }
+
+  //Generate the SoC
+  def main(args: Array[String]): Unit = {
+    SpinalRtlConfig.generateVerilog(IceStormInOutWrapper(default(new TinyFpgaBxSocMinimal()).toComponent()))
+  }
+}
+
---
+++ hardware/scala/saxon/board/tinyfpgabx/genpll.sh
@@ -0,0 +1,1 @@
+icepll -i 16 -o 20 -f tinyfpgabx_pll.v -m -n tinyfpgabx_pll
