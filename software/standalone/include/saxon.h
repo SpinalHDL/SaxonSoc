f1512439e23d642b9abcb9a16b812678a9ba7fcb
lawrie <lawrie.griffiths@ntlworld.com>
2019-06-06 20:20:14 +0100

    Use 0x10000000 as IO address for blinkAndEcho

--- software/standalone/include/saxon.h
+++ software/standalone/include/saxon.h
@@ -4,10 +4,10 @@
 #include "gpio.h"
 #include "uart.h"
 
-#define CORE_HZ 12000000
+#define CORE_HZ 25000000
 
-#define GPIO_A    ((Gpio_Reg*)(0xF0000000))
-#define UART_A      ((Uart_Reg*)(0xF0010000))
+#define GPIO_A    ((Gpio_Reg*)(0x10000000))
+#define UART_A      ((Uart_Reg*)(0x10010000))
 
 
 #endif 
