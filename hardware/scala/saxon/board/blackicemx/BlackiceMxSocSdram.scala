d04bb8891aabe051e7f36b493ab3acfac7964471
lawrie <lawrie.griffiths@ntlworld.com>
2019-07-05 21:37:34 +0100

    First go at Blackice Mx SDRAM

---
+++ hardware/scala/saxon/board/blackicemx/BlackiceMxSocSdram.scala
@@ -0,0 +1,165 @@
+package saxon.board.blackicemx
+
+import saxon._
+import spinal.core._
+import spinal.lib.com.uart.UartCtrlMemoryMappedConfig
+import spinal.lib.generator._
+import spinal.lib.io.{Gpio, InOutWrapper}
+import spinal.lib.com.jtag.sim.JtagTcp
+import spinal.lib.com.uart.sim.{UartDecoder, UartEncoder}
+import saxon.board.blackice._
+import saxon.board.blackice.peripheral._
+import spinal.lib.memory.sdram._
+import spinal.lib.memory.sdram.sim.SdramModel
+
+object IS42S16100H {
+  def layout = SdramLayout(
+    bankWidth   = 1,
+    columnWidth = 8,
+    rowWidth    = 11,
+    dataWidth   = 16
+  )
+
+  def timingGrade7 = SdramTimings(
+    bootRefreshCount =   8,
+    tPOW             = 100 us,
+    tREF             =  64 ms,
+    tRC              =  60 ns,
+    tRFC             =  60 ns,
+    tRAS             =  37 ns,
+    tRP              =  15 ns,
+    tRCD             =  15 ns,
+    cMRD             =   2,
+    tWR              =  10 ns,
+    cWR              =   1
+  )
+}
+
+class BlackiceMxSocSdramSystem extends BmbApbVexRiscvGenerator{
+  //Add components
+  val ramA = BmbOnChipRamGenerator(0x80000000l)
+  val uartA = Apb3UartGenerator(0x10000)
+  val gpioA = Apb3GpioGenerator(0x00000)
+  val sdramA = SdramSdrBmbGenerator(0x90000000l)
+
+  ramA.dataWidth.load(32)
+
+  //Interconnect specification
+  interconnect.addConnection(
+    cpu.iBus -> List(ramA.bmb, sdramA.bmb),
+    cpu.dBus -> List(ramA.bmb, sdramA.bmb, peripheralBridge.input)
+  )
+
+  interconnect.setConnector(sdramA.bmb){(m,s) =>
+    m.cmd >-> s.cmd
+    m.rsp << s.rsp
+  }
+}
+
+
+class BlackiceMxSocSdram extends Generator{
+  val clockCtrl = ClockDomainGenerator()
+  clockCtrl.resetHoldDuration.load(255)
+  clockCtrl.powerOnReset.load(true)
+  clockCtrl.clkFrequency.load(25 MHz)
+
+  val system = new BlackiceMxSocSdramSystem
+  system.onClockDomain(clockCtrl.clockDomain)
+
+  val clocking = add task new Area{
+    val clk_25M = in Bool()
+
+    clockCtrl.clock.load(clk_25M)
+  }
+}
+
+object BlackiceMxSocSdramSystem{
+  def default(g : BlackiceMxSocSdramSystem, clockCtrl : ClockDomainGenerator) = g {
+    import g._
+
+    cpu.config.load(VexRiscvConfigs.minimal)
+    cpu.enableJtag(clockCtrl)
+
+    ramA.size.load(8 KiB)
+    ramA.hexInit.load("software/standalone/blinkAndEcho/build/blinkAndEcho.hex")
+
+    sdramA.layout.load(IS42S16100H.layout)
+    sdramA.timings.load(IS42S16100H.timingGrade7)
+
+    uartA.parameter load UartCtrlMemoryMappedConfig(
+      baudrate = 115200,
+      txFifoDepth = 1,
+      rxFifoDepth = 1
+    )
+
+    gpioA.parameter load Gpio.Parameter(width = 8)
+
+    g
+  }
+}
+
+
+object BlackiceMxSocSdram {
+  //Function used to configure the SoC
+  def default(g : BlackiceMxSocSdram) = g{
+    import g._
+    BlackiceMxSocSdramSystem.default(system, clockCtrl)
+    clockCtrl.resetSensitivity load(ResetSensitivity.NONE)
+    g
+  }
+
+  //Generate the SoC
+  def main(args: Array[String]): Unit = {
+    val report = SpinalRtlConfig.generateVerilog(IceStormInOutWrapper(default(new BlackiceMxSocSdram()).toComponent()))
+    BspGenerator("BlackiceMxSocSdram", report.toplevel.generator, report.toplevel.generator.system.cpu.dBus)
+  }
+}
+
+object BlackiceMxSocSdramSystemSim {
+  import spinal.core.sim._
+
+  def main(args: Array[String]): Unit = {
+
+    val simConfig = SimConfig
+    simConfig.allOptimisation
+    simConfig.withWave
+    simConfig.compile(new BlackiceMxSocSdramSystem(){
+      val clockCtrl = ClockDomainGenerator()
+      this.onClockDomain(clockCtrl.clockDomain)
+      clockCtrl.makeExternal(ResetSensitivity.FALL)
+      clockCtrl.powerOnReset.load(true)
+      clockCtrl.clkFrequency.load(25 MHz)
+      BlackiceMxSocSdramSystem.default(this, clockCtrl)
+    }.toComponent()).doSimUntilVoid("test", 42){dut =>
+      val systemClkPeriod = (1e12/dut.clockCtrl.clkFrequency.toDouble).toLong
+      val jtagClkPeriod = systemClkPeriod*4
+      val uartBaudRate = 115200
+      val uartBaudPeriod = (1e12/uartBaudRate).toLong
+
+      val clockDomain = ClockDomain(dut.clockCtrl.clock, dut.clockCtrl.reset)
+      clockDomain.forkStimulus(systemClkPeriod)
+
+      val tcpJtag = JtagTcp(
+        jtag = dut.cpu.jtag,
+        jtagClkPeriod = jtagClkPeriod
+      )
+
+      val uartTx = UartDecoder(
+        uartPin =  dut.uartA.uart.txd,
+        baudPeriod = uartBaudPeriod
+      )
+
+      val uartRx = UartEncoder(
+        uartPin = dut.uartA.uart.rxd,
+        baudPeriod = uartBaudPeriod
+      )
+
+      val sdram = SdramModel(
+        io = dut.sdramA.sdram,
+        layout = dut.sdramA.logic.layout,
+        clockDomain = clockDomain
+      )
+    }
+  }
+}
+
