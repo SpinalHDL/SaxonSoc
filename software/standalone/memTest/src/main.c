a33897f5c09e23c4c02a9d4680036dd3f00d9593
lawrie <lawrie.griffiths@ntlworld.com>
2019-07-07 11:26:30 +0100

    Added memTest software

---
+++ software/standalone/memTest/src/main.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+#include "saxon.h"
+
+#define mem ((volatile uint32_t*)0x90000000)
+#define MAX_WORDS (512 * 1014)
+
+void main() {
+
+    GPIO_A->OUTPUT_ENABLE = 0x000000FF;
+
+    // Set the Blue led to indicate test in progress
+    GPIO_A->OUTPUT = 0x000000E0;
+    
+    for(int i=0;i<MAX_WORDS;i++) mem[i] = i;
+
+    for(int i=0;i<MAX_WORDS;i++) {
+      if (mem[i] != i) {
+        // Set the Red leed for failure
+        GPIO_A->OUTPUT = 0x00000070;
+        while(1){}
+      }
+    }
+
+    // Set the green Led for success
+    GPIO_A->OUTPUT = 0x000000D0;
+    while(1){}
+}
+
