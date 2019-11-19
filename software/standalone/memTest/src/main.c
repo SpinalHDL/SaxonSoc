5c299dcc4a50f4f9737c249722e61bb3c426985e
lawrie <lawrie.griffiths@ntlworld.com>
2019-11-19 09:15:38 +0000

    Added uart messages to memTest

--- software/standalone/memTest/src/main.c
+++ software/standalone/memTest/src/main.c
@@ -1,11 +1,25 @@
 #include <stdint.h>
 
 #include "saxon.h"
+#include "io.h"
+
+void print_hex(uint32_t val, uint32_t digits)
+{
+	for (int i = (4*digits)-4; i >= 0; i -= 4)
+		uart_write(UART_A, "0123456789ABCDEF"[(val >> i) % 16]);
+}
+
+
+void print(uint8_t * data) {
+  uart_writeStr(UART_A, data);
+}
 
 #define mem ((volatile uint32_t*)0x90000000)
-#define MAX_WORDS (512 * 1024)
+#define MAX_WORDS (8 * 1024 * 1024)
 
 void main() {
+
+    print("Memory test\n");
 
     GPIO_A->OUTPUT_ENABLE = 0x000000FF;
 
@@ -18,12 +32,20 @@ void main() {
       if (mem[i] != i) {
         // Set the Red led for failure
         GPIO_A->OUTPUT = 0x00000007;
+	print("Failed at address 0x");
+	print_hex(i, 8);
+	print(" with value 0x");
+	print_hex(mem[i], 8);
+	print("\n");
+
         while(1){}
       }
     }
 
     // Set the Green led for success
     GPIO_A->OUTPUT = 0x0000000D;
+    print("Success\n");
+
     while(1){}
 }
 
