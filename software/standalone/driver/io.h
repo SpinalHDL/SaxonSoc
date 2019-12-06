b8671cbe73c9c6dbf6965b3bca35050126ecd0af
Charles Papon <charles.papon.90@gmail.com>
2019-12-06 01:24:00 +0100

    io.h now implement a optional udelay based on the machine timer

--- software/standalone/driver/io.h
+++ software/standalone/driver/io.h
@@ -4,29 +4,34 @@
 #include <stdint.h>
 #include "soc.h"
 
-inline uint32_t read_u32(uint32_t address){
+static inline uint32_t read_u32(uint32_t address){
 	return *((volatile uint32_t*) address);
 }
 
-inline void write_u32(uint32_t data, uint32_t address){
+static inline void write_u32(uint32_t data, uint32_t address){
 	*((volatile uint32_t*) address) = data;
 }
 
-inline void write_u32_ad(uint32_t address, uint32_t data){
+static inline void write_u32_ad(uint32_t address, uint32_t data){
 	*((volatile uint32_t*) address) = data;
 }
 
-void io_delay(uint32_t cycles){
-	//TODO
-	/*
-	for(int32_t i = (cycles + 7) >> 3;i >= 0;i--){
-		asm("nop;nop;nop;nop;nop;nop;nop;nop;");
-	}*/
-}
+//static void io_delay(uint32_t cycles){
+//	//TODO
+//	/*
+//	for(int32_t i = (cycles + 7) >> 3;i >= 0;i--){
+//		asm("nop;nop;nop;nop;nop;nop;nop;nop;");
+//	}*/
+//}
 
-void io_udelay(uint32_t usec){
-	io_delay(usec*200); //TODO
+#if defined(SYSTEM_MACHINE_TIMER_APB)
+static void io_udelay(uint32_t usec){
+	uint32_t mTimePerUsec = SYSTEM_MACHINE_TIMER_HZ/1000000;
+	uint32_t limit = read_u32(SYSTEM_MACHINE_TIMER_APB) + usec*mTimePerUsec;
+	while((int32_t)(limit-(read_u32(SYSTEM_MACHINE_TIMER_APB))) >= 0);
 }
+#endif
+
 
 #endif
 
