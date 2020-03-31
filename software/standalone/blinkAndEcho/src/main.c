cea8d03082c9249ae919accf38fa0967a15c6642
Dolu1990 <charles.papon.90@gmail.com>
2020-03-31 11:47:42 +0200

    Fix blinkAndEcho

--- software/standalone/blinkAndEcho/src/main.c
+++ software/standalone/blinkAndEcho/src/main.c
@@ -4,6 +4,12 @@
 #include "gpio.h"
 #include "uart.h"
 
+#ifdef SPINAL_SIM
+#define LOOP_UDELAY 100
+#else
+#define LOOP_UDELAY 100000
+#endif
+
 void main() {
     bsp_init();
 
@@ -11,19 +17,14 @@ void main() {
     gpio_setOutput(BSP_LED_GPIO, 0x00000000);
 
     uart_write(BSP_UART_TERMINAL, '!');
-
-    uint32_t counter = 0;
     while(1){
-        if(counter++ == 1000){
-            gpio_setOutput(BSP_LED_GPIO, gpio_getInput(BSP_LED_GPIO) ^ BSP_LED_MASK);
-            counter = 0;
-        }
+        gpio_setOutput(BSP_LED_GPIO, gpio_getOutput(BSP_LED_GPIO) ^ BSP_LED_MASK);
 
         while(uart_readOccupancy(BSP_UART_TERMINAL)){
             uart_write(BSP_UART_TERMINAL, uart_read(BSP_UART_TERMINAL));
         }
 
-        bsp_uDelay(100000);
+        bsp_uDelay(LOOP_UDELAY);
     }
 }
 
