61795f07efdae0a6e7158ddfb5cd02af37748f3b
roman3017 <rbacik@hotmail.com>
2020-06-19 09:48:26 -0700

    Fix MII dataWidth in NexysA7SmpLinux

--- hardware/scala/saxon/board/digilent/NexysA7SmpLinux.scala
+++ hardware/scala/saxon/board/digilent/NexysA7SmpLinux.scala
@@ -3,6 +3,7 @@ package saxon.board.digilent
 import saxon.{ResetSensitivity, _}
 import spinal.core._
 import spinal.lib.bus.misc.{AddressMapping, SizeMapping}
+import spinal.lib.com.eth.{MacEthParameter, Mii, MiiParameter, MiiRxParameter, MiiTxParameter, PhyParameter}
 import spinal.lib.com.jtag.sim.JtagTcp
 import spinal.lib.com.uart.sim.{UartDecoder, UartEncoder}
 import spinal.lib.io.{Gpio, InOutWrapper}
@@ -15,7 +16,9 @@ object NexysA7SmpLinux {
     import g._
 
     sdramDomain.phyA.sdramLayout.load(MT47H64M16HR.layout)
-    ArtyA7SmpLinuxSystem.default(system, debugCd, sdramCd).sdramA.coreParameter.load(CoreParameter(
+    ArtyA7SmpLinuxSystem.default(system, debugCd, sdramCd)
+
+    system.sdramA.coreParameter.load(CoreParameter(
       portTockenMin = 4,
       portTockenMax = 8,
       timingWidth = 4,
@@ -26,7 +29,19 @@ object NexysA7SmpLinux {
       readLatencies = List(5+3, 5+4)
     ))
 
+    system.mac.parameter load MacEthParameter(
+      phy = PhyParameter(
+        txDataWidth = 2,
+        rxDataWidth = 2
+      ),
+      rxDataWidth = 32,
+      rxBufferByteSize = 4096,
+      txDataWidth = 32,
+      txBufferByteSize = 4096
+    )
+
     system.ramA.hexInit.load("software/standalone/bootloader/build/bootloader.hex")
+
     g
   }
 
@@ -38,104 +53,5 @@ object NexysA7SmpLinux {
       ).addStandardMemBlackboxing(blackboxByteEnables)
        .generateVerilog(InOutWrapper(default(new ArtyA7SmpLinux()).toComponent()))
     BspGenerator("digilent/NexysA7SmpLinux", report.toplevel.generator, report.toplevel.generator.system.cores(0).cpu.dBus)
-  }
-}
-
-object NexysA7SmpLinuxSystemSim {
-  import spinal.core.sim._
-
-  def main(args: Array[String]): Unit = {
-
-    val simConfig = SimConfig
-    simConfig.allOptimisation
-    simConfig.withWave
-    simConfig.withFstWave
-    simConfig.addSimulatorFlag("-Wno-MULTIDRIVEN")
-
-    simConfig.compile(new ArtyA7SmpLinuxSystem(){
-      val debugCd = ClockDomainResetGenerator()
-      debugCd.enablePowerOnReset()
-      debugCd.holdDuration.load(63)
-      debugCd.makeExternal(
-        frequency = FixedFrequency(100 MHz)
-      )
-
-      val systemCd = ClockDomainResetGenerator()
-      systemCd.holdDuration.load(63)
-      systemCd.setInput(debugCd)
-
-      this.onClockDomain(systemCd.outputClockDomain)
-
-      val phy = RtlPhyGenerator()
-      phy.layout.load(XilinxS7Phy.phyLayout(MT47H64M16HR.layout, 2))
-      phy.connect(sdramA)
-
-      sdramA.mapCtrlAt(0x100000)
-      interconnect.addConnection(bmbPeripheral.bmb, sdramA.ctrlBus)
-
-      val bridge = JtagTapDebuggerGenerator() onClockDomain(debugCd.outputClockDomain)
-      for(i <- 0 until cpuCount) {
-        cores(i).cpu.enableDebugBmb(debugCd, systemCd, SizeMapping(0x10B80000 + i*0x1000, 0x1000))
-        interconnect.addConnection(bridge.bmb, cores(i).cpu.debugBmb)
-      }
-
-      ArtyA7SmpLinuxSystem.default(this, debugCd, systemCd)
-      ramA.hexInit.load("software/standalone/bootloader/build/bootloader_spinal_sim.hex")
-    }.toComponent()).doSimUntilVoid("test", 42){dut =>
-      val debugClkPeriod = (1e12/dut.debugCd.inputClockDomain.frequency.getValue.toDouble).toLong
-      val jtagClkPeriod = debugClkPeriod*4
-      val uartBaudRate = 115200
-      val uartBaudPeriod = (1e12/uartBaudRate).toLong
-
-      val clockDomain = dut.debugCd.inputClockDomain.get
-      clockDomain.forkStimulus(debugClkPeriod)
-
-      fork{
-        val at = 0
-        val duration = 0
-        while(simTime() < at*1000000000l) {
-          disableSimWave()
-          sleep(100000 * 10000)
-          enableSimWave()
-          sleep(  100 * 10000)
-        }
-        println("\n\n********************")
-        println("********************\n\n")
-        sleep(duration*1000000000l)
-        while(true) {
-          disableSimWave()
-          sleep(100000 * 10000)
-          enableSimWave()
-          sleep(  100 * 10000)
-        }
-      }
-
-      val tcpJtag = JtagTcp(
-        jtag = dut.bridge.jtag,
-        jtagClkPeriod = jtagClkPeriod
-      )
-
-      val uartTx = UartDecoder(
-        uartPin =  dut.uartA.uart.txd,
-        baudPeriod = uartBaudPeriod
-      )
-
-      val uartRx = UartEncoder(
-        uartPin = dut.uartA.uart.rxd,
-        baudPeriod = uartBaudPeriod
-      )
-
-      val uboot = "../u-boot/"
-      val opensbi = "../opensbi/build/platform/spinal/saxon/digilent/artyA7Smp/firmware/"
-      val linuxPath = "../buildroot/output/images/"
-
-      dut.phy.logic.loadBin(0x00F80000, opensbi + "fw_jump.bin")
-      dut.phy.logic.loadBin(0x00F00000, uboot + "u-boot.bin")
-      dut.phy.logic.loadBin(0x00000000, linuxPath + "uImage")
-      dut.phy.logic.loadBin(0x00FF0000, linuxPath + "dtb")
-      dut.phy.logic.loadBin(0x00FFFFC0, linuxPath + "rootfs.cpio.uboot")
-
-      println("DRAM loading done")
-    }
   }
 }
--- hardware/synthesis/nexys_a7/NexysA7.xdc
+++ hardware/synthesis/nexys_a7/NexysA7.xdc
@@ -108,10 +108,10 @@ set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets system_eth_mii_TX_CLK_IBUF];
 
 #set_property -dict { PACKAGE_PIN D14   IOSTANDARD LVCMOS33 } [get_ports { JB[1] }]; #IO_L1P_T0_AD0P_15 Sch=jb[1]
 #set_property -dict { PACKAGE_PIN F16   IOSTANDARD LVCMOS33 } [get_ports { JB[2] }]; #IO_L14N_T2_SRCC_15 Sch=jb[2]
-set_property -dict { PACKAGE_PIN G16   IOSTANDARD LVCMOS33 } [get_ports { system_eth_mii_TX_D[2] }]; #IO_L13N_T2_MRCC_15 Sch=jb[3]
-set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { system_eth_mii_TX_D[3] }]; #IO_L15P_T2_DQS_15 Sch=jb[4]
-set_property -dict { PACKAGE_PIN E16   IOSTANDARD LVCMOS33 } [get_ports { system_eth_mii_RX_D[2] }]; #IO_L11N_T1_SRCC_15 Sch=jb[7]
-set_property -dict { PACKAGE_PIN F13   IOSTANDARD LVCMOS33 } [get_ports { system_eth_mii_RX_D[3] }]; #IO_L5P_T0_AD9P_15 Sch=jb[8]
+#set_property -dict { PACKAGE_PIN G16   IOSTANDARD LVCMOS33 } [get_ports { JB[3] }]; #IO_L13N_T2_MRCC_15 Sch=jb[3]
+#set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { JB[4] }]; #IO_L15P_T2_DQS_15 Sch=jb[4]
+#set_property -dict { PACKAGE_PIN E16   IOSTANDARD LVCMOS33 } [get_ports { JB[7] }]; #IO_L11N_T1_SRCC_15 Sch=jb[7]
+#set_property -dict { PACKAGE_PIN F13   IOSTANDARD LVCMOS33 } [get_ports { JB[8] }]; #IO_L5P_T0_AD9P_15 Sch=jb[8]
 #set_property -dict { PACKAGE_PIN G13   IOSTANDARD LVCMOS33 } [get_ports { JB[9] }]; #IO_0_15 Sch=jb[9]
 #set_property -dict { PACKAGE_PIN H16   IOSTANDARD LVCMOS33 } [get_ports { JB[10] }]; #IO_L13P_T2_MRCC_15 Sch=jb[10]
 
