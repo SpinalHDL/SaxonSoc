833a244140f1448d076aaedef7a417b6dc0b0352
roman3017 <rbacik@hotmail.com>
2020-09-26 08:56:46 -0700

    NexysA7 vga and audio fixes

--- bsp/digilent/NexysA7SmpLinux/app/vgaInit.h
+++ bsp/digilent/NexysA7SmpLinux/app/vgaInit.h
@@ -4,26 +4,27 @@
 #include "dmasg.h"
 #include "vga.h"
 
-#define DMASG_BASE SYSTEM_DMA_CTRL
+#define DMASG_BASE           SYSTEM_DMA_CTRL
 
-#define VGA_BASE SYSTEM_VGA_CTRL
-#define VGA_CHANNEL     0
-#define VGA_PORT     0
-#define VGA_SINK 0
+#define VGA_BASE             SYSTEM_VGA_CTRL
+#define VGA_CHANNEL          0
+#define VGA_PORT             0
+#define VGA_SINK             0
 
 #ifdef SPINAL_SIM
-    #define VGA_TIMINGS vga_simRes_h160_v120
-    #define VGA_WIDTH 160
-    #define VGA_HEIGHT 120
+    #define VGA_TIMINGS      vga_simRes_h160_v120
+    #define VGA_WIDTH        160
+    #define VGA_HEIGHT       120
 #else
-    #define VGA_TIMINGS vga_h640_v480_r60
-    #define VGA_WIDTH 640
-    #define VGA_HEIGHT 480
+    #define VGA_TIMINGS      vga_h640_v480_r60
+    #define VGA_WIDTH        640
+    #define VGA_HEIGHT       480
 #endif
 
-#define VGA_BYTE_PER_PIXEL 2
-#define VGA_BUFFER     0x8F000000
-#define VGA_BUFFER_SIZE (VGA_WIDTH * VGA_HEIGHT * VGA_BYTE_PER_PIXEL)
+#define VGA_BYTE_PER_PIXEL   2
+#define VGA_BUFFER           0x87000000
+#define VGA_BUFFER_SIZE      0x01000000
+//#define VGA_BUFFER_SIZE     (VGA_WIDTH * VGA_HEIGHT * VGA_BYTE_PER_PIXEL)
 
 void vgaInit(){
     bsp_putString("VGA init\n");
@@ -38,8 +39,6 @@ void vgaInit(){
         }
     }
     dmasg_input_memory(DMASG_BASE, VGA_CHANNEL,  (u32)VGA_BUFFER, 0);
-    dmasg_output_stream (DMASG_BASE, VGA_CHANNEL, VGA_PORT, 0, VGA_SINK, 1);
+    dmasg_output_stream(DMASG_BASE, VGA_CHANNEL, VGA_PORT, 0, VGA_SINK, 1);
     dmasg_direct_start(DMASG_BASE, VGA_CHANNEL, VGA_BUFFER_SIZE, 1);
 }
-
-
