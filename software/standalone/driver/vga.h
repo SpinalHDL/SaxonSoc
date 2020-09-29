99669db68bf2575461228f8cd0f5ffa906d22a98
roman3017 <rbacik@hotmail.com>
2020-09-28 18:51:33 -0700

    VgaCtrl move polarity regs to the end
     - for backward compatibility

--- software/standalone/driver/vga.h
+++ software/standalone/driver/vga.h
@@ -6,11 +6,11 @@
 typedef struct {
 	u32 hSyncStart ,hSyncEnd;
 	u32 hColorStart,hColorEnd;
-	u32 hPolarity;
 
 	u32 vSyncStart ,vSyncEnd;
 	u32 vColorStart,vColorEnd;
-	u32 vPolarity;
+
+	u32 polarities;
 }Vga_Timing;
 
 static const Vga_Timing vga_h640_v480_r60 = {
@@ -18,12 +18,11 @@ static const Vga_Timing vga_h640_v480_r60 = {
     .hSyncEnd    = 800,
     .hColorStart = 96 + 16,
     .hColorEnd   = 800 - 48,
-    .hPolarity   = 0,
     .vSyncStart  = 2,
     .vSyncEnd 	 = 525,
     .vColorStart = 2 + 10,
     .vColorEnd 	 = 525 - 33,
-    .vPolarity   = 0,
+    .polarities  = 0,
 };
 
 static const Vga_Timing vga_h800_v600_r72 = {
@@ -31,12 +30,11 @@ static const Vga_Timing vga_h800_v600_r72 = {
     .hSyncEnd    = 1040,
     .hColorStart = 120 + 56,
     .hColorEnd   = 1040 - 64,
-    .hPolarity   = 1,
     .vSyncStart  = 6,
     .vSyncEnd 	 = 666,
     .vColorStart = 6 + 37,
     .vColorEnd 	 = 666 - 23,
-    .vPolarity   = 1,
+    .polarities  = 3,
 };
 
 static const Vga_Timing vga_h800_v600_r60 = {
@@ -44,12 +42,11 @@ static const Vga_Timing vga_h800_v600_r60 = {
     .hSyncEnd    = 1056,
     .hColorStart = 128 + 40,
     .hColorEnd   = 1056 - 88,
-    .hPolarity   = 1,
     .vSyncStart  = 4,
     .vSyncEnd 	 = 628,
     .vColorStart = 4 + 1,
     .vColorEnd 	 = 628 - 23,
-    .vPolarity   = 1,
+    .polarities  = 3,
 };
 
 static const Vga_Timing vga_simRes = {
@@ -57,12 +54,11 @@ static const Vga_Timing vga_simRes = {
     .hSyncEnd    = 70,
     .hColorStart = 16,
     .hColorEnd   = 64,
-    .hPolarity   = 0,
     .vSyncStart  = 2,
     .vSyncEnd 	 = 48,
     .vColorStart = 8,
     .vColorEnd 	 = 40,
-    .vPolarity   = 0,
+    .polarities  = 0,
 };
 
 static const Vga_Timing vga_simRes_h160_v120 = {
@@ -70,12 +66,11 @@ static const Vga_Timing vga_simRes_h160_v120 = {
 	.hSyncEnd    = 24+160,
 	.hColorStart = 16,
 	.hColorEnd   = 16+160,
-    .hPolarity   = 0,
 	.vSyncStart  = 2,
 	.vSyncEnd 	 = 10+120,
 	.vColorStart = 6,
 	.vColorEnd 	 = 6+120,
-    .vPolarity   = 0,
+    .polarities  = 0,
 };
 
 static void vga_start(u32 base){
@@ -90,10 +85,9 @@ static void vga_set_timing(u32 base, Vga_Timing t){
     write_u32(t.hSyncEnd    , base + 0x44);
     write_u32(t.hColorStart , base + 0x48);
     write_u32(t.hColorEnd   , base + 0x4C);
-    write_u32(t.hPolarity   , base + 0x50);
-    write_u32(t.vSyncStart  , base + 0x54);
-    write_u32(t.vSyncEnd    , base + 0x58);
-    write_u32(t.vColorStart , base + 0x5C);
-    write_u32(t.vColorEnd   , base + 0x60);
-    write_u32(t.vPolarity   , base + 0x64);
+    write_u32(t.vSyncStart  , base + 0x50);
+    write_u32(t.vSyncEnd    , base + 0x54);
+    write_u32(t.vColorStart , base + 0x58);
+    write_u32(t.vColorEnd   , base + 0x5C);
+    write_u32(t.polarities  , base + 0x60);
 }
