8031a060882e05f64fc9c9bc3d74efbf1a311ca5
Dolu1990 <charles.papon.90@gmail.com>
2021-03-29 19:34:42 +0200

    Ulx3sMinimal setup SoC interconnect and add sim

--- hardware/scala/saxon/board/radiona/ulx3s/Ulx3sMinimal.scala
+++ hardware/scala/saxon/board/radiona/ulx3s/Ulx3sMinimal.scala
@@ -3,11 +3,26 @@ package saxon.board.radiona.ulx3s
 import saxon._
 import spinal.core._
 import spinal.lib.com.uart.UartCtrlMemoryMappedConfig
+import spinal.lib.bus.bmb._
+import spinal.lib.bus.misc.SizeMapping
+import spinal.lib.com.jtag.sim.JtagTcp
+import spinal.lib.com.uart.sim.{UartDecoder, UartEncoder}
 import spinal.lib.generator._
 import spinal.lib.io.{Gpio, InOutWrapper}
+import spinal.lib.misc.plic.PlicMapping
+import vexriscv.VexRiscvBmbGenerator
 
 // Define a SoC abstract enough to be used for simulation
-class Ulx3sMinimalAbstract extends BmbApbVexRiscvGenerator{
+class Ulx3sMinimalAbstract extends Area{
+  implicit val interconnect = BmbInterconnectGenerator()
+  implicit val bmbPeripheral = BmbBridgeGenerator(mapping = SizeMapping(0x10000000, 16 MiB)).peripheral(dataWidth = 32)
+  implicit val peripheralDecoder = bmbPeripheral.asPeripheralDecoder() //Will be used by peripherals as default bus to connect to
+  implicit val cpu = VexRiscvBmbGenerator()
+
+  interconnect.setDefaultArbitration(BmbInterconnectGenerator.STATIC_PRIORITY)
+  interconnect.setPriority(cpu.iBus, 1)
+  interconnect.setPriority(cpu.dBus, 2)
+
   //Add components
   val ramA = BmbOnChipRamGenerator(0x80000000l)
   val gpioA = BmbGpioGenerator(0x00000)
@@ -18,7 +33,7 @@ class Ulx3sMinimalAbstract extends BmbApbVexRiscvGenerator{
   //Interconnect specification
   interconnect.addConnection(
     cpu.iBus -> List(ramA.ctrl),
-    cpu.dBus -> List(ramA.ctrl, peripheralBridge.input)
+    cpu.dBus -> List(ramA.ctrl, bmbPeripheral.bmb)
   )
 }
 
@@ -42,6 +57,9 @@ class Ulx3sMinimal extends Component{
 
   val system = systemCd on new Ulx3sMinimalAbstract(){
     cpu.enableJtag(debugCdCtrl, systemCdCtrl)
+
+    interconnect.setPipelining(bmbPeripheral.bmb)(cmdHalfRate = true, rspHalfRate = true)
+    interconnect.setPipelining(cpu.dBus)(cmdValid = true)
   }
 }
 
@@ -66,7 +84,7 @@ object Ulx3sMinimalAbstract{
   }
 }
 
-object Ulx3sMinimal {
+object Ulx3sMinimal extends App{
   //Function used to configure the SoC
   def default(g : Ulx3sMinimal) = g.rework {
     import g._
@@ -75,9 +93,49 @@ object Ulx3sMinimal {
   }
 
   //Generate the SoC
-  def main(args: Array[String]): Unit = {
-    val report = SpinalRtlConfig.generateVerilog(InOutWrapper(default(new Ulx3sMinimal())))
-    BspGenerator("Ulx3sMinimal", report.toplevel, report.toplevel.system.cpu.dBus)
+  val report = SpinalRtlConfig.generateVerilog(InOutWrapper(default(new Ulx3sMinimal())))
+  BspGenerator("Ulx3sMinimal", report.toplevel, report.toplevel.system.cpu.dBus)
+}
+
+
+object Ulx3sMinimalSim extends App{
+  import spinal.core.sim._
+
+  val config = SimConfig
+  config.compile{
+    val dut = new Ulx3sMinimal()
+    Ulx3sMinimal.default(dut) //Configure the system
+    dut
+  }.doSimUntilVoid(seed=42){dut =>
+    val debugClkPeriod = (1e12/dut.debugCdCtrl.inputClockDomain.frequency.getValue.toDouble).toLong
+    val jtagClkPeriod = debugClkPeriod*8
+    val uartBaudRate = 115200
+    val uartBaudPeriod = (1e12/uartBaudRate).toLong
+
+    dut.debugCdCtrl.inputClockDomain.forkStimulus(debugClkPeriod)
+
+    JtagTcp(
+      jtag = dut.system.cpu.jtag,
+      jtagClkPeriod = jtagClkPeriod
+    )
+
+    UartDecoder(
+      uartPin =  dut.system.uartA.uart.txd,
+      baudPeriod = uartBaudPeriod
+    )
+
+    UartEncoder(
+      uartPin = dut.system.uartA.uart.rxd,
+      baudPeriod = uartBaudPeriod
+    )
   }
 }
 
+//  val plic = BmbPlicGenerator(0xC00000)
+//  plic.priorityWidth.load(2)
+//  plic.mapping.load(PlicMapping.sifive)
+//  plic.addTarget(cpu.externalInterrupt)
+//
+//  val clint = BmbClintGenerator(0xB00000)
+//  clint.cpuCount.load(1)
+//  cpu.setTimerInterrupt(clint.timerInterrupt(0))
