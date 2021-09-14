4975d98b87a3728b304dd106b16740a556fe6a7e
Dolu1990 <charles.papon.90@gmail.com>
2021-09-14 10:05:02 +0200

    arty better mdio bootloader

--- bsp/digilent/ArtyA7SmpLinux/app/bootloaderConfig.h
+++ bsp/digilent/ArtyA7SmpLinux/app/bootloaderConfig.h
@@ -5,6 +5,7 @@
 #include "sdram.h"
 #include "spiFlash.h"
 #include "vgaInit.h"
+#include "mdio.h"
 
 #define SDRAM_CTRL SYSTEM_SDRAM_A_CTRL
 #define SDRAM_PHY  SDRAM_DOMAIN_PHY_A_CTRL
@@ -12,6 +13,7 @@
 
 #define SPI SYSTEM_SPI_A_CTRL
 #define SPI_CS 0
+#define MD_CS 2
 
 #define GPIO SYSTEM_GPIO_A_CTRL
 
@@ -27,6 +29,7 @@
 #define WL 5
 #define CTRL_BURST_LENGHT 2
 #define PHY_CLK_RATIO 2
+
 
 void bspMain() {
     bsp_putString("\n");
@@ -65,6 +68,48 @@ void bspMain() {
 
     vgaInit();
 #endif
+
+    // MDIO
+    bsp_putString("Configuring MDIO\n");
+
+    u32 clkDivider = BSP_CLINT_HZ/(1000000*2)-1;
+
+    Spi_Config spiCfg;
+    spiCfg.cpol = 0;
+    spiCfg.cpha = 0;
+    spiCfg.mode = 1;
+    spiCfg.clkDivider = clkDivider;
+    spiCfg.ssSetup = clkDivider;
+    spiCfg.ssHold = clkDivider;
+    spiCfg.ssDisable = clkDivider;
+    spi_applyConfig(SPI, &spiCfg);
+
+    bsp_putString("  control (old): ");
+    u16 control = mdio_read(SPI, MD_CS, 1, 0);
+    bsp_putU32(control);
+    bsp_putString("\n");
+
+    // Set 100Mbps and auto-negotiate
+    mdio_write(SPI, MD_CS, 1, 4, 0x0181);
+    mdio_write(SPI, MD_CS, 1, 0, 0x1000);
+    #ifndef SPINAL_SIM
+    bsp_uDelay(10000);
+    #endif
+
+    bsp_putString("  control (new): ");
+    control = mdio_read(SPI, MD_CS, 1, 0);
+    bsp_putU32(control);
+    bsp_putString("\n");
+
+    bsp_putString("  status: ");
+    control = mdio_read(SPI, MD_CS, 1, 1);
+    bsp_putU32(control);
+    bsp_putString("\n");
+
+    bsp_putString("  status 4: ");
+    control = mdio_read(SPI, MD_CS, 1, 4);
+    bsp_putU32(control);
+    bsp_putString("\n");
 
     bsp_putString("Payload boot\n");
     void (*userMain)(u32, u32, u32) = (void (*)(u32, u32, u32))OPENSBI_MEMORY;
