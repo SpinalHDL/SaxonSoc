ecfc3ea55bdee17ba86733d0169181a245a2f285
Dolu1990 <charles.papon.90@gmail.com>
2022-05-24 12:33:22 +0200

    vexriscv.h commenting

--- software/standalone/driver/vexriscv.h
+++ software/standalone/driver/vexriscv.h
@@ -2,13 +2,18 @@
 
 #include "riscv.h"
 
+//Invalidate the whole data cache
 #define data_cache_invalidate_all() asm(".word(0x500F)");
-#define data_cache_invalidate_address(rs1)     \
+
+//Invalidate all the data cache ways lines which could store the given address
+#define data_cache_invalidate_address(address)     \
 ({                                             \
     asm volatile(                              \
      ".word ((0x500F) | (regnum_%0 << 15));"   \
      :                                         \
-     : "r" (rs1)                               \
+     : "r" (address)                               \
     );                                         \
 })
+
+//Invalidate the whole instruction cache
 #define instruction_cache_invalidate() asm("fence.i");
